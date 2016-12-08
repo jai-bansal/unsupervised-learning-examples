90c6f8830d38b27e9f7873f282e443fdff170206
jai-bansal <jbans79@gmail.com>
2016-12-07 16:20:49 -0800

    finished ica section
    finished ica section and added estimated source signal plots. need to
    check if data should be scaled and then this script is done

--- component_analysis/ica.py
+++ component_analysis/ica.py
@@ -113,3 +113,60 @@ plt.show()
 ################################
 # INDEPENDENT COMPONENT ANALYSIS
 ################################
+# This section conducts independent component analysis.
+
+# Conduct ICA.
+ica = FastICA(n_components = 2,
+              random_state = 8583).fit(mixed_signals)
+
+# View estimated mixing matrix.
+# This is a bit different than the actual mixing matrix 'mixing',
+# but similar to the estimated mixing matrix in the R branch.
+ica.mixing_
+
+# Reconstruct source signals.
+estimated_signals = pd.DataFrame(ica.transform(mixed_signals))
+estimated_signals.columns = ['e1', 'e2']
+
+# Plot the source signal estimates found by ICA.
+# The estimate for source signal 1 looks pretty good,
+# but not so much for the estimate for source signal 2.
+# The results are very similar to the plotted estimates in the R branch.
+
+# Plot source signal 1 estimate.
+
+# Create figure and subplot.
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+
+# Add line data.
+ax1.plot(range(1, 101),
+         estimated_signals['e1'],
+         color = 'black')
+
+# Set title and axes labels.
+plt.title('Source Signal 1 Estimate')
+plt.xlabel('')
+plt.ylabel('Source Signal 1 Estimate')
+
+# Show plot.
+plt.show()
+
+# Plot source signal 2 estimate.
+
+# Create figure and subplot.
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+
+# Add line data.
+ax1.plot(range(1, 101),
+         estimated_signals['e2'],
+         color = 'black')
+
+# Set title and axes labels.
+plt.title('Source Signal 2 Estimate')
+plt.xlabel('')
+plt.ylabel('Source Signal 2 Estimate')
+
+# Show plot.
+plt.show()
