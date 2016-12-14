9d7046b4a35cb5bc0ba800757dc56f6a21a9a324
jai-bansal <jbans79@gmail.com>
2016-12-13 17:42:04 -0800

    create framework for association rules
    create framework for association rules

---
+++ association_rules/association_rules.py
@@ -0,0 +1,16 @@
+# This script conducts association rule analysis on generated data.
+
+################
+# IMPORT MODULES
+################
+# This section imports relevant modules.
+
+###############
+# GENERATE DATA
+###############
+# This section generates the data to be used for association rules analysis.
+
+########################################
+# SUMMARY AND ASSOCIATION RULES ANALYSIS
+########################################
+# This section conducts summary and association rule analysis.
